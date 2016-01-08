f578e69ba377e2a39f977f3c0dabe3a662f9304f
Allexandriya <anastasiia.pidvalna@gmail.com>
2016-01-08 01:09:45 -0800

    Add and styled new products + single product view. Add column Price and Color to the db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107080953) do
+ActiveRecord::Schema.define(version: 20160108080611) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20160107080953) do
     t.string   "link"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "color"
+    t.integer  "price"
   end
 
 end
