577b6171d19956f49c03bd0cf9ff3ba460143595
Allexandriya <anastasiia.pidvalna@gmail.com>
2016-01-23 01:54:42 -0800

    Contact form added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,14 @@ class StaticPagesController < ApplicationController
   	@products = Product.all
   end
 
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+
+  	ActionMailer::Base.mail(:from => @email, 
+  		:to => 'anastasiia.pidvalna@gmail.com',
+  		:subject => 'A new contact form message from #{@name}',
+  		:body => @message).deliver
+  end
 end
