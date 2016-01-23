f7d7934bedda313bf9850d6b64525db04000c76a
Allexandriya <anastasiia.pidvalna@gmail.com>
2016-01-23 02:53:35 -0800

    Styling HTML email form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,10 +7,11 @@ class StaticPagesController < ApplicationController
   	@name = params[:name]
   	@email = params[:email]
   	@message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver
 
   	ActionMailer::Base.mail(:from => @email, 
   		:to => 'anastasiia.pidvalna@gmail.com',
-  		:subject => 'A new contact form message from #{@name}',
+  		:subject => "A new contact form message from " + @name,
   		:body => @message).deliver
   end
 end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "contacts@anastasiiasbikes.com"
+  layout 'mailer'
+end
