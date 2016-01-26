cb2bf7bf438df1f20e0472ca83bf1c709e0e1250
Allexandriya <anastasiia.pidvalna@gmail.com>
2016-01-26 08:25:17 -0800

    Create comment model and set up relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125155526) do
+ActiveRecord::Schema.define(version: 20160126162232) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.integer  "rating"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["product_id"], name: "index_comments_on_product_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "controllers", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "users"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "controllers", ["email"], name: "index_controllers_on_email", unique: true
+  add_index "controllers", ["reset_password_token"], name: "index_controllers_on_reset_password_token", unique: true
 
   create_table "orders", force: true do |t|
     t.integer "user_id"
