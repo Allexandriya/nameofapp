046594c6e47f520a474ce227ad0a4ead6bcf9d56
Allexandriya <s.allexandriya@gmail.com>
2016-02-17 06:32:52 -0800

    Processing payments created by the Stripe

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,40 @@
+class PaymentsController < ApplicationController
+	
+  # POST /payments
+  # POST /payments.json
+	def create
+		@product = Product.find(params[:product_id])
+		@user = current_user
+		token = params[:stripeToken]
+		# Create the charge on Stripe's servers - this will charge the user's card
+		begin
+			charge = Stripe::Charge.create(
+				:amount => @product.price * 100, #in cents
+				:currency => 'usd'
+				:source => token
+				:description => params[:stripeEmail]
+			)
+
+			if charge.paid
+				Order.create(
+					:product_id => @product.id
+					:user_id => @user.id
+					:total => @product.price
+				)
+			end
+
+			rescue Stripe::CardError => e
+		      	#card has been declined
+		      	body = e.json_body
+		      	err = body[:error]
+		      	flash[:error] = "Sorry, your card was declined with the error: #{err[:message]}"
+	    	end
+
+    	end
+
+		respond_to do |format|
+	        format.html { redirect_to @user, notice: 'User was successfully created.' }
+	        format.json { render :show, status: :created, location: @user }   
+	    end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
+  post 'payments/create' => 'products#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
